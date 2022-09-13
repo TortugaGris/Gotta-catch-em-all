fd9fc7e2c1e18b2f9b8eda11fbff1d96387eb260
Javier Zavala <javierzavalatorres@gmail.com>
2022-09-13 15:05:46 -0300

    build: add angular fire, bootstrap and primeNG

---
+++ gotta-cath-em-all/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+const config = {
+  apiKey: "AIzaSyC-zfmP__i_E68tdjcE3ZPUuKu-mwiaSIA",
+  authDomain: "gotta-catch-em-all-353fe.firebaseapp.com",
+  projectId: "gotta-catch-em-all-353fe",
+  storageBucket: "gotta-catch-em-all-353fe.appspot.com",
+  messagingSenderId: "377017776579",
+  appId: "1:377017776579:web:c78538252027db312f2576"
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
