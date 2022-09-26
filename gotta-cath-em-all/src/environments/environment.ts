ceba810ef09c2b766478608b731fb982479032ce
Javier Zavala <javierzavalatorres@gmail.com>
2022-09-26 18:51:04 -0300

    fix: add firabase credentials

---
+++ gotta-cath-em-all/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC-zfmP__i_E68tdjcE3ZPUuKu-mwiaSIA",
+    authDomain: "gotta-catch-em-all-353fe.firebaseapp.com",
+    projectId: "gotta-catch-em-all-353fe",
+    storageBucket: "gotta-catch-em-all-353fe.appspot.com",
+    messagingSenderId: "377017776579",
+    appId: "1:377017776579:web:c78538252027db312f2576"
+  } 
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
