5e5cbec360b6f4952408d59b0aa72d15cf8f72fc
Javier Zavala <javierzavalatorres@gmail.com>
2022-09-26 08:09:40 -0300

    refactor: delete console logs and old functions

--- gotta-cath-em-all/src/app/captures-view/captures-view.component.ts
+++ gotta-cath-em-all/src/app/captures-view/captures-view.component.ts
@@ -4,7 +4,6 @@ import { AuthService } from '../services/auth.service';
 import { filter, Observable, tap, switchMap, map } from 'rxjs';
 import { User } from '../services/user.model';
 import { ICaptureList } from '../interfaces/ICaptureList';
-import { FieldValue, Timestamp } from 'firebase/firestore';
 
 @Component({
   selector: 'app-captures-view',
@@ -21,24 +20,7 @@ export class CapturesViewComponent implements OnInit {
       filter((user): user is User => !!user),
       tap((user) => this.uid = user.uid),
       switchMap((user) => this.pokemonCapture.getUserCaptures(user.uid)))
-    // .subscribe(
-    //   user => this.captures$ = this.pokemonCapture.getUserCaptures(user.uid),
-    //   err => console.error(err))
   }
 
-  ngOnInit(): void {
-  }
-  
-  ngOnDestroy(): void {
-  }
-
-  asTimestamp(date: (()=>FieldValue) | FieldValue | Timestamp | undefined) {
-    if (date) return (date as Timestamp)
-    else return null
-  }
-
-  print(value: object) {
-    console.log(value)
-  }
-
+  ngOnInit(): void {}
 }
