a2f92fbc2069fc72682f52a4ae8c0aa8311b266c
Javier Zavala <javierzavalatorres@gmail.com>
2022-09-26 08:05:55 -0300

    doc: add documentation for services functions

--- gotta-cath-em-all/src/app/services/pokemon-capture.service.ts
+++ gotta-cath-em-all/src/app/services/pokemon-capture.service.ts
@@ -9,14 +9,25 @@ import { ICaptureList } from '../interfaces/ICaptureList'
 export class PokemonCaptureService {
   constructor(private db: AngularFirestore) {}
 
+  /**
+   * Add Pokemon to the captures collection
+   */
   addPokemonCapture(pokemonCapture: IPokemonCapture) {
     this.db.collection('captures').add(pokemonCapture);
   }
 
+  /**
+   * Delete a Pokemon in the captures collection using the document ID
+   * @param capturesId - Document ID
+   */
   deletePokemonCapture(capturesId:string) {
     this.db.collection('captures').doc(capturesId).delete();
   }
 
+  /**
+   * Get list of Pokemons captured by the user
+   * @param userId - User ID
+   */
   getUserCaptures(userId:string) {
     return this.db.collection<ICaptureList>('captures',
                                                ref => ref.where('userId', '==', userId))
