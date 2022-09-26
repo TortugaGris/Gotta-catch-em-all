c0e7a0c00cf05f0e863c5e9f110d781c1e48747d
Javier Zavala <javierzavalatorres@gmail.com>
2022-09-26 09:02:12 -0300

    refactor: delete code not being used

--- gotta-cath-em-all/src/app/captures-view/captures-view.component.ts
+++ gotta-cath-em-all/src/app/captures-view/captures-view.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonCaptureService } from '../services/pokemon-capture.service';
 import { AuthService } from '../services/auth.service'; 
-import { filter, Observable, tap, switchMap, map } from 'rxjs';
+import { filter, Observable, switchMap } from 'rxjs';
 import { User } from '../services/user.model';
 import { ICaptureList } from '../interfaces/ICaptureList';
 
@@ -11,14 +11,12 @@ import { ICaptureList } from '../interfaces/ICaptureList';
   styleUrls: ['./captures-view.component.scss']
 })
 export class CapturesViewComponent implements OnInit {
-  uid="";
   captures$: Observable<ICaptureList[]>;
 
   constructor(public pokemonCapture: PokemonCaptureService,
               private auth: AuthService) { 
     this.captures$ = this.auth.getUser().pipe(
       filter((user): user is User => !!user),
-      tap((user) => this.uid = user.uid),
       switchMap((user) => this.pokemonCapture.getUserCaptures(user.uid)))
   }
 
