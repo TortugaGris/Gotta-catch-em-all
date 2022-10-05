f88d494a61e286c14dfe2229ce1d8a11ca684fa0
Paths <pcampaa@gmail.com>
2022-10-05 11:41:19 -0300

    fix(pokemon-service) update getRandomPokemon to use an internal observable

--- gotta-cath-em-all/src/app/services/pokemon.service.ts
+++ gotta-cath-em-all/src/app/services/pokemon.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { IPokemonList } from '../interfaces/IPokemonList';
-import { IPokemon } from '../interfaces/IPokemon';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, switchMap} from 'rxjs';
+import {IPokemonList} from '../interfaces/IPokemonList';
+import {IPokemon} from '../interfaces/IPokemon';
 
 
 @Injectable({
@@ -10,21 +10,26 @@ import { IPokemon } from '../interfaces/IPokemon';
 })
 
 export class PokemonService {
-  private _url: string = "https://pokeapi.co/api/v2/pokemon"
-  private pokemonList: IPokemonList = {count: 1, results: [{url: this._url + '/1'}]};
+  private _url: string = "https://pokeapi.co/api/v2/pokemon";
 
-  constructor(private http:HttpClient) { 
-    let pokemon$ = this.http.get<IPokemonList>(this._url + '?limit=10000&offset=0');
-    pokemon$.subscribe(data => this.pokemonList = data)
+  private watchPokemonList(): Observable<IPokemonList> {
+    return this.http.get<IPokemonList>(this._url + '?limit=10000&offset=0');
   }
-  
+
+  constructor(private http: HttpClient) {
+  }
+
   /**
    * Get a random pokemon from the pokemons available at pokeapi
    * @returns Observable of the pokemon's information
    */
-  getRandomPokemon(): Observable<IPokemon>{
-    let randomIndex = Math.floor(Math.random() * this.pokemonList.count);
-    let url = this.pokemonList.results[randomIndex].url
-    return this.http.get<IPokemon>(url);
+  getRandomPokemon(): Observable<IPokemon> {
+    return this.watchPokemonList().pipe(
+      switchMap(pokemonList => {
+        let randomIndex = Math.floor(Math.random() * pokemonList.count);
+        let url = pokemonList.results[randomIndex].url
+        return this.http.get<IPokemon>(url);
+      })
+    )
   }
 }
